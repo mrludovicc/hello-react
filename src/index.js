5391f693d9b68004aed15ad3663f7fa350dfe292
mrludovicc <nadal46@hotmail.com>
2023-03-20 18:39:38 -0400

    Add full solution

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ import './index.css';
 
 const Button = (props) => {
   // your code here
+
+  return (
+    <button onClick={props.handleOnClick}>{props.text}</button>
+  )
 };
 
 const Application = () => {
-
+  const [name, setName] = useState('')
   // your code here
-
-  const reset = () => {
+  const handleOnChange = (e) => {
+    setName(e.target.value);
+  }
+  const reset = (e) => {
+    setName(e.target.value = '')
     console.log("reset");
     // your code here
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input type="text" name="name" placeholder='TYPE YOUR NAME' setName={setName} value={name} onChange={handleOnChange} />
+      <Button text='RESET' handleOnClick={reset} />
+      {name.length > 0 && <h1>Hello {name}</h1>}
     </main>
   );
 };
